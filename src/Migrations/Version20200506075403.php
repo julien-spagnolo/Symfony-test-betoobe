4898e91699d3e1b8975704c8ad4924ba6b7a649e
julien <julien.devone@gmail.com>
2020-05-06 09:55:13 +0200

    add entities User and Activity

---
+++ src/Migrations/Version20200506075403.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200506075403 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, activity_id INT DEFAULT NULL, first_name VARCHAR(30) NOT NULL, last_name VARCHAR(40) NOT NULL, email VARCHAR(60) NOT NULL, birthdate DATE NOT NULL, is_blocked TINYINT(1) NOT NULL, roles JSON NOT NULL, INDEX IDX_8D93D64981C06096 (activity_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE activity (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, start_at DATETIME NOT NULL, end_at DATETIME NOT NULL, age_require INT NOT NULL, maximum_people INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64981C06096 FOREIGN KEY (activity_id) REFERENCES activity (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D64981C06096');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE activity');
+    }
+}
